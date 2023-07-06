66c62aa33edf725ed90bcb0894e69004bb35479e
“UnityGitApps” <nikcxprojects@gmail.com>
2023-07-06 09:43:20 +0300

    update

---
+++ Assets/Third/Scripts/OneSignalInitializer.cs
@@ -0,0 +1,39 @@
+using OneSignalSDK;
+using UnityEngine;
+
+public class OneSignalInitializer : MonoBehaviour
+{
+
+    private void Start()
+    {
+        // Enable lines below to debug issues with OneSignal
+        OneSignal.Default.LogLevel = LogLevel.Info;
+        OneSignal.Default.AlertLevel = LogLevel.Fatal;
+
+        // Setup the below to listen for and respond to events from notifications
+        OneSignal.Default.NotificationOpened += _notificationOpened;
+        OneSignal.Default.NotificationWillShow += _notificationReceived;
+
+        OneSignal.Default.Initialize("e0073d6d-a5e1-4408-8f06-7ac262e3aa52");
+    }
+
+    private void _log(object message)
+    {
+        Debug.Log(message);
+    }
+
+    private void _notificationOpened(NotificationOpenedResult result)
+    {
+        _log($"Notification was opened with result: {JsonUtility.ToJson(result)}");
+    }
+
+    private Notification _notificationReceived(Notification notification)
+    {
+        var additionalData = notification.additionalData != null
+            ? Json.Serialize(notification.additionalData)
+                : null;
+
+        _log($"Notification was received in foreground: {JsonUtility.ToJson(notification)}\n{additionalData}");
+        return notification; // show the notification
+    }
+}
